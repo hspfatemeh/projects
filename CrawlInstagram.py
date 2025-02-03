c4b26c0b38126f8923d77382709b5dd9e3268a4f
hspfatemeh <51415176+hspfatemeh@users.noreply.github.com>
2025-02-03 21:37:57 +0330

    Update CrawlInstagram.py

--- CrawlInstagram.py
+++ CrawlInstagram.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By  # Import By module
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -33,14 +31,14 @@ password.clear()
 
 # Enter credentials
 username.send_keys("fatihhhhhh26")
-password.send_keys("))))))))")
+password.send_keys("----")
 
 # Find and click the login button
 login_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
 login_button.click()
 
 # After login
-time.sleep(10)
+time.sleep(3)
 
 try:
     not_now_button = WebDriverWait(driver, 20).until(
@@ -63,62 +61,60 @@ except Exception as e:
     print(f"Error clicking 'Not Now' button (notifications): {str(e)}")
 
 
-time.sleep(2)  # Wait a few seconds for the home page to load
+time.sleep(1)  # Wait a few seconds for the home page to load
 
 # Go to your profile page using your username
 profile_url = f"https://www.instagram.com/fatihhhhhh26/"
 driver.get(profile_url)
 
-time.sleep(3)  # Adjust this time as needed
+time.sleep(2)  # Adjust this time as needed
 
 # Click on 'Followings' to open the following list
 followings_button = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.XPATH, "//a[contains(@href, '/following')]"))
 )
 followings_button.click()
+time.sleep(4)
 
-# Wait for the following list to load
-time.sleep(5)
 
-# Get the following dialog element
-followings_container = driver.find_element(By.XPATH, "//div[@role='dialog']")
+followings_container = driver.find_element(By.XPATH, "/html/body/div[5]/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]")
 
-# Initialize an empty list to store all following links
+
 following_urls = set()  # Use a set to avoid duplicates
 previous_links_count = 0
 
-# Keep scrolling until no new links are added
 while True:
-    # Scroll down the dialog to load more followings
-    driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", followings_container)
-    time.sleep(3)  # Wait for new followings to load
 
+    driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", followings_container)
+    time.sleep(3) 
+    driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", followings_container)
+    time.sleep(3)
+    driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", followings_container)
+    time.sleep(3)
     # Get all the 'a' tags inside the dialog
     following_links = driver.find_elements(By.XPATH, "//div[@role='dialog']//a")
 
     # Add the unique URLs to the set
     for link in following_links:
         url = link.get_attribute("href")
-        if url and "/p/" not in url:  # Ignore non-profile links
+        if url and "/p/" not in url:  
             following_urls.add(url)
 
-    # Check if the number of links has increased
     if len(following_urls) == previous_links_count:
-        break  # Stop if no new links have been added
+        break  
     previous_links_count = len(following_urls)
 
-# Convert the set of following URLs to a list
 following_urls = list(following_urls)
+print(len(following_urls))
 
-# Initialize a list to store followings' bios
+
 followings_bios = []
 
 # Loop through each following and get their bio
 for url in following_urls:
     driver.get(url)
-    time.sleep(3)  # Wait for the profile to load
+    time.sleep(3)  
 
-    # We now wait for the bio element to be available in the DOM
     try:
         # Fetch the bio, checking for the "more" button
         bio_element = WebDriverWait(driver, 10).until(
